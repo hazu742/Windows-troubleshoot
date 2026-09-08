081f588087cfb09dc9e90f13e77a4c9a54688474
hazu742 <burungghantuu@gmail.com>
2026-09-08 22:43:11 +0700

    Add files via upload

---
+++ CTT WinUtil  hazu script.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command ""irm https://christitus.com/win | iex""'"
